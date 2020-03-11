cde904ddf1d27b6aa339d0816540eea6e8d732ed
Jonah Fisher <50385193+JonahF95@users.noreply.github.com>
2020-03-11 11:42:12 -0400

    Documentation and data for saliva estimates object

--- R/BeadSorted.Saliva.EPIC.estimates.R
+++ R/BeadSorted.Saliva.EPIC.estimates.R
@@ -1,6 +1,16 @@
 #' @name BeadSorted.Saliva.EPIC.estimates
 #' @docType data
-#' @title Estimated cell composition for 12 whole saliva samples
-#' @format A data frame with 12 rows and 2 variables
-
+#' @title Estimated cell composition for samples
+#' @format A data frame with 60 rows and 2 variables
+#' \describe{
+#' \item{sampid}{Sample identification number}
+#' \item{immuneCells}{Estimated proportion of immune cells in sample}
+#' \item{epithelialCells}{Estimate proportion of epithelial cells in sample}
+#' }
+#'
+#'
+#'
+#'
+#'
+"BeadSorted.Saliva.EPIC.estimates"
 
