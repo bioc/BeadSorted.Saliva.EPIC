e4d5392afb210a3dbda152db6c8f4c737a079ede
Jonah Fisher <50385193+JonahF95@users.noreply.github.com>
2020-03-11 11:44:04 -0400

    update var count

--- R/BeadSorted.Saliva.EPIC.estimates.R
+++ R/BeadSorted.Saliva.EPIC.estimates.R
@@ -1,7 +1,7 @@
 #' @name BeadSorted.Saliva.EPIC.estimates
 #' @docType data
 #' @title Estimated cell composition for samples
-#' @format A data frame with 60 rows and 2 variables
+#' @format A data frame with 60 rows and 3 variables
 #' \describe{
 #' \item{sampid}{Sample identification number}
 #' \item{immuneCells}{Estimated proportion of immune cells in sample}
